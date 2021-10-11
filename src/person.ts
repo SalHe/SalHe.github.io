41bddbb29025ccfb672efb038b1e6329678eadb6
SalHe Li <SalHe@qq.com>
2021-10-12 00:20:32 +0800

    fix a grammatical error

--- src/person.ts
+++ src/person.ts
@@ -4,7 +4,7 @@ export const profile = {
     realName: "SalHe Li",
     nickName: "SalHe",
     slogan: "I wanna be free.",
-    description: "An university student majoring in Computer Science in Wuhan University. Prefer some classic games such as Bomber-Man, Contra and Metal Slug. Dream to be a developer with enthusiasm and without constrain.",
+    description: "A university student majoring in Computer Science in Wuhan University. Prefer some classic games such as Bomber-Man, Contra and Metal Slug. Dream to be a developer with enthusiasm and without constrain.",
     major: "Computer Science",
     school: "Wuhan University",
     email: "SalHe@qq.com",
