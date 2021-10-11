5c7b912b0f1009b5197744c92e8dad5976e5f374
SalHe <SalHe@qq.com>
2021-10-11 15:03:33 +0800

    using WebHistory instead of WebHashHistory

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import Blog from "./views/Blog.vue";
 import Post from "./views/Post.vue";
 
 export const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         {
             path: "/",
